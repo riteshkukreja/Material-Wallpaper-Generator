352cf9087f86cf5ac8c854f6d74a9c2e6278c305
Ritesh <ritesh@mettl.com>
2018-07-01 11:25:03 +0530

    Added complementary colors
    Provides better visual effect when using complementary colors instead of
    similar colors

--- MaterialImageGenerator.js
+++ MaterialImageGenerator.js
@@ -110,6 +110,31 @@ var MaterialImageGenerator = function(config) {
 		return val % offset;
 	}
 
+	var invertColor = function(hex) {
+		if (hex.indexOf('#') === 0) {
+			hex = hex.slice(1);
+		}
+		// convert 3-digit hex to 6-digits.
+		if (hex.length === 3) {
+			hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
+		}
+		if (hex.length !== 6) {
+			throw new Error('Invalid HEX color.');
+		}
+		// invert color components
+		var r = (255 - parseInt(hex.slice(0, 2), 16)).toString(16),
+			g = (255 - parseInt(hex.slice(2, 4), 16)).toString(16),
+			b = (255 - parseInt(hex.slice(4, 6), 16)).toString(16);
+		// pad each with zeros and return
+		return '#' + padZero(r) + padZero(g) + padZero(b);
+	}
+	
+	var padZero = function(str, len) {
+		len = len || 2;
+		var zeros = new Array(len).join('0');
+		return (zeros + str).slice(-len);
+	}
+
 	var similarColor = function(color) {
 		// color is in hex form #rrggbb
 		var offset = 50;
@@ -201,10 +226,11 @@ var MaterialImageGenerator = function(config) {
 		svg.innerHTML = "";
 		var time = count;
 		var op = ops[getRandomInt(0, ops.length)];
-		var color = Math.random() > 0.5 ? randomColor(): similarColor(baseColor);
+		var lastColor = baseColor;
 
 		for(var i = 0; i < time; i++) {
-			push(op(color));
+			lastColor = Math.random() > 0.5 ? randomColor(): invertColor(lastColor);
+			push(op(lastColor));
 		}
 
 		buildImage();
